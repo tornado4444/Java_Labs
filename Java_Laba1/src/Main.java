7d5ae972be161ad069e477382c2e881b500ecc74
Tornado_Killer <tornadokiller4444@gmail.com>
2025-09-08 10:52:25 +0300

    Lol

--- Java_Laba1/src/Main.java
+++ Java_Laba1/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] argv){
         // 1.1 Завдання
-        int i;
+        int i = 0;
         char c = 'A';
         boolean f1 = false;
         boolean f2 = true;
